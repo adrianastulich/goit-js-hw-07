9c1bfd84dc9f51c656ffe733882b011933c3634e
Adriana Stulich <ada.stulich@gmail.com>
2023-03-09 21:19:13 +0100

    done hw 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const cardsMarkup = createGalleryCardsMarkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onGalleryItemClick)
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `
+    })
+    .join('')
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault()
+
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+
+  modalOpen(event)
+}
+
+
+
+function modalOpen(event) {
+  const galleryItemOriginal = event.target.dataset.source
+
+  const modal = basicLightbox.create(`<img src="${galleryItemOriginal}" width="1024" height="600">`)
+  modal.show()
+
+  function closeModalOnEsc(event) {
+    if (event.code !== 'Escape') {
+      return
+    }
+    console.log('key');
+    modal.close()
+    window.removeEventListener('keydown', closeModalOnEsc);
+  }
+  window.addEventListener('keydown', closeModalOnEsc)
+}
