f24cc7151a613aaf965a0c69b485a5146b6b93e8
Adriana Stulich <ada.stulich@gmail.com>
2023-03-09 22:11:58 +0100

    done hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery')
-const cardsMarkup = createGalleryCardsMarkup(galleryItems)
-
-galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-galleryContainer.addEventListener('click', onGalleryItemClick)
-
-function createGalleryCardsMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-      <div class="gallery__item">
+const gallerySection = document.querySelector('.gallery')
+const markup = galleryItems
+    .map(({ preview, original, description }) => 
+      `<div class="gallery__item">
         <a class="gallery__link" href="large-image.jpg">
           <img
             class="gallery__image"
@@ -20,32 +13,29 @@ function createGalleryCardsMarkup(galleryItems) {
             alt="${description}"
           />
         </a>
-      </div>
-    `
-    })
+      </div>`)
     .join('')
-}
 
-function onGalleryItemClick(event) {
-  event.preventDefault()
 
-  if (event.target.nodeName !== 'IMG') {
+gallerySection.insertAdjacentHTML('afterbegin', markup);
+gallerySection.addEventListener('click', onImageClick)
+
+
+function onImageClick(e) {
+  e.preventDefault()
+  if (e.target.nodeName !== 'IMG') {
     return
   }
-
-  modalOpen(event)
+  modalOpen(e)
 }
 
-
-
-function modalOpen(event) {
-  const galleryItemOriginal = event.target.dataset.source
-
+function modalOpen(e) {
+  const galleryItemOriginal = e.target.dataset.source
   const modal = basicLightbox.create(`<img src="${galleryItemOriginal}" width="1024" height="600">`)
   modal.show()
 
-  function closeModalOnEsc(event) {
-    if (event.code !== 'Escape') {
+  function closeModalOnEsc(e) {
+    if (e.code !== 'Escape') {
       return
     }
     console.log('key');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallerySection = document.querySelector('.gallery')
+const markup = galleryItems
+    .map(({ preview, original, description }) => 
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`)
+    .join('')
+
+
+gallerySection.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.9,
+    captionsData: 'alt',
+    captionDelay: 400,
+    animationSpeed: 400,
+
+
+});
